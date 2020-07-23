fcfd7b546ff00f5587e6ee5ba0bb39e059c16ee8
andrin.meier <andrin.meier@bmpi.ch>
2020-07-23 17:07:16 +0200

    optimize vorstand page

--- src/templates/vorstand-page.tsx
+++ src/templates/vorstand-page.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { graphql, StaticQuery } from 'gatsby'
+import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
+import Img from 'gatsby-image'
 
 const VorstandPage = ({ data }: { data: any }) => {
   const { edges: members } = data.allMarkdownRemark;
@@ -17,24 +18,26 @@ const VorstandPage = ({ data }: { data: any }) => {
               <div className="section">
                 <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
                   Vorstand
-          </h1>{members &&
+          </h1><div className="tile is-ancestor">{members &&
                   members.map(({ node: member }: { node: any }) => (
-                    <div className="card">
-                      <div className="card-image">
-                        <figure className="image is-square">
-                          <PreviewCompatibleImage imageInfo={member.frontmatter.portrait} />
-                        </figure>
-                      </div>
-                      <div className="card-content">
-                        <div className="media">
-                          <div className="media-content">
-                            <p className="title is-4">{member.frontmatter.title}</p>
-                            <p className="subtitle is-6">{member.frontmatter.funktion}</p>
+                    <div className="tile">
+                      <div className="card">
+                        <div className="card-image">
+                          <figure className="image">
+                            <Img fixed={member.frontmatter.portrait.image.childImageSharp.fixed} alt={member.frontmatter.portrait.alt} />
+                          </figure>
+                        </div>
+                        <div className="card-content">
+                          <div className="media">
+                            <div className="media-content">
+                              <p className="title is-4">{member.frontmatter.title}</p>
+                              <p className="subtitle is-6">{member.frontmatter.funktion}</p>
+                            </div>
                           </div>
                         </div>
                       </div>
                     </div>
-                  ))}
+                  ))}</div>
               </div>
             </div>
           </div>
@@ -70,8 +73,8 @@ export const vorstandPageQuery = graphql`
                   alt
                   image {
                     childImageSharp {
-                      fluid(maxWidth: 400, quality: 92) {
-                        ...GatsbyImageSharpFluid
+                      fixed(width: 400, quality: 92) {
+                        ...GatsbyImageSharpFixed
                       }
                     }
                   }
