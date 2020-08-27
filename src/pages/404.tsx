8fd2f7d38a99184a5ab745ebed5310fbe192c7e2
andrin.meier <andrin.meier@bmpi.ch>
2020-08-27 14:30:56 +0200

    fix accessibility issues

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,10 +3,18 @@ import Layout from '../components/Layout'
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
-      <h1>SEITE NICHT GEFUNDEN</h1>
-      <p>Diese Seite existiert leider nicht.</p>
-    </div>
+    <section className="section">
+      <div className="container content">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <div className="section">
+              <h1 className="title is-size-2 has-text-weight-bold is-bold-light">Seite nicht gefunden</h1>
+              <p>Die Seite konnte nicht gefunden werden.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </Layout>
 )
 
