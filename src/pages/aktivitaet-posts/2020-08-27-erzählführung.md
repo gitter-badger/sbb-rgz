5a35ccb47399df074fe31de33df93b9f1b9c52f3
Andrin Meier <andrin.meier@bmpi.ch>
2020-08-27 21:38:29 +0200

    add anmeldeformular to aktivitäten

--- src/pages/aktivitaet-posts/2020-08-27-erzählführung.md
+++ src/pages/aktivitaet-posts/2020-08-27-erzählführung.md
@@ -2,6 +2,7 @@
 templateKey: aktivitaet-post
 title: Erzählführung
 date: 2020-09-20T11:44:01.058Z
+anmeldeformularanzeigen: true
 ---
 Führung durch die Stadt Zürich mit Erzählungen, Geräuschen und Düften.
 
--- src/templates/aktivitaet-post.tsx
+++ src/templates/aktivitaet-post.tsx
@@ -2,23 +2,55 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Helmet } from 'react-helmet'
 import { graphql } from 'gatsby'
+import { navigate } from 'gatsby-link'
 import Layout from '../components/Layout'
+import { useState } from 'react';
 import Content, { HTMLContent } from '../components/Content'
+
+function encode(data: any) {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
 
 export const AktivitaetPostTemplate = ({
   content,
   contentComponent,
   title,
   date,
+  anmeldeformularanzeigen,
+  slug,
   helmet,
 }: {
   content: any,
   contentComponent: any,
   title: any,
   date: any,
+  anmeldeformularanzeigen: any,
+  slug: any,
   helmet: any,
 }) => {
   const PostContent = contentComponent || Content
+  const [count, setCount] = useState({});
+
+  function handleChange(e: any) {
+    setCount({ [e.target.name]: e.target.value })
+  }
+
+  function handleSubmit(e: any) {
+    e.preventDefault()
+    const form = e.target
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...count,
+      }),
+    })
+      .then(() => navigate(form.getAttribute('action')))
+      .catch((error) => alert(error))
+  }
 
   return (
     <section className="section">
@@ -27,9 +59,83 @@ export const AktivitaetPostTemplate = ({
         <div className="columns">
           <div className="column is-10 is-offset-1">
             <h1 className="title is-size-1">
-            {date} - {title}
+              {date} - {title}
             </h1>
             <PostContent content={content} />
+            {anmeldeformularanzeigen &&
+              <div>
+                <br/>
+                <h2 className="title is-size-2">
+                  Anmeldeformular
+              </h2>
+                <form
+                  name={slug}
+                  method="post"
+                  action="/kontakt/danke/"
+                  data-netlify="true"
+                  data-netlify-honeypot="bot-field"
+                  onSubmit={handleSubmit}
+                >
+                  {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+                  <input type="hidden" name="form-name" value="contact" />
+                  <div hidden>
+                    <label>
+                      Nicht ausfüllen:{' '}
+                      <input name="bot-field" onChange={handleChange} />
+                    </label>
+                  </div>
+                  <div className="field">
+                    <label className="label" htmlFor={'name'}>
+                      Name
+            </label>
+                    <div className="control">
+                      <input
+                        className="input"
+                        type={'text'}
+                        name={'name'}
+                        onChange={handleChange}
+                        id={'name'}
+                        required={true}
+                      />
+                    </div>
+                  </div>
+                  <div className="field">
+                    <label className="label" htmlFor={'email'}>
+                      Email
+            </label>
+                    <div className="control">
+                      <input
+                        className="input"
+                        type={'email'}
+                        name={'email'}
+                        onChange={handleChange}
+                        id={'email'}
+                        required={true}
+                      />
+                    </div>
+                  </div>
+                  <div className="field">
+                    <label className="label" htmlFor={'bemerkung'}>
+                      Bemerkung
+            </label>
+                    <div className="control">
+                      <textarea
+                        className="textarea"
+                        name={'bemerkung'}
+                        onChange={handleChange}
+                        id={'bemerkung'}
+                        required={true}
+                      />
+                    </div>
+                  </div>
+                  <div className="field">
+                    <button className="button" type="submit">
+                      Anmelden
+            </button>
+                  </div>
+                </form>
+              </div>
+            }
           </div>
         </div>
       </div>
@@ -63,6 +169,8 @@ const AktivitaetPost = ({ data }: { data: any }) => {
         }
         title={post.frontmatter.title}
         date={post.frontmatter.date}
+        anmeldeformularanzeigen={post.frontmatter.anmeldeformularanzeigen}
+        slug={post.fields.slug}
       />
     </Layout>
   )
@@ -81,9 +189,13 @@ export const pageQuery = graphql`
     markdownRemark(id: { eq: $id }) {
       id
       html
+      fields {
+        slug
+      }
       frontmatter {
         date(formatString: "DD.MM.YYYY")
         title
+        anmeldeformularanzeigen
       }
     }
   }
