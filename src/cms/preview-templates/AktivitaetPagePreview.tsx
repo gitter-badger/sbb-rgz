5120684f1be562b2ed4cae449cffe0a37761e616
Andrin Meier <andrin.meier@bmpi.ch>
2020-08-28 16:59:03 +0200

    fix wrong function call

--- src/cms/preview-templates/AktivitaetPagePreview.tsx
+++ src/cms/preview-templates/AktivitaetPagePreview.tsx
@@ -6,7 +6,7 @@ const AktivitaetPagePreview = ({ entry, widgetFor }: {entry: any, widgetFor: any
   <AktivitaetPostTemplate
     title={entry.getIn(['data', 'title'])}
     content={widgetFor('body')}
-    date={entry.getIn(moment(['data', 'date'])).format("DD.MM.YYYY")}
+    date={moment(entry.getIn(['data', 'date'])).format("DD.MM.YYYY")}
     anmeldeformularanzeigen={entry.getIn(['data', 'anmeldeformularanzeigen'])}
   />
 )
