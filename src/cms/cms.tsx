462b0da4e1a0c1019f5ddcdb43f14c9d057d84d5
Andrin Meier <andrin.meier@bmpi.ch>
2020-08-28 13:19:26 +0200

    netlify cms preview template for aktivitäten

--- src/cms/cms.tsx
+++ src/cms/cms.tsx
@@ -1,6 +1,9 @@
 import CMS from 'netlify-cms-app'
+import AktivitaetPagePreview from './preview-templates/AktivitaetPagePreview'
 var uploadcare = require('netlify-cms-media-library-uploadcare')
 var cloudinary = require('netlify-cms-media-library-cloudinary')
 
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
+
+CMS.registerPreviewTemplate('aktivitaeten', AktivitaetPagePreview)
---
+++ src/cms/preview-templates/AktivitaetPagePreview.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { AktivitaetPostTemplate } from '../../templates/aktivitaet-post'
+
+const AktivitaetPagePreview = ({ entry, widgetFor }: {entry: any, widgetFor: any}) => (
+  <AktivitaetPostTemplate
+    title={entry.getIn(['data', 'title'])}
+    content={widgetFor('body')}
+    contentComponent={undefined}
+    date={entry.getIn(['data', 'date'])}
+    anmeldeformularanzeigen={entry.getIn(['data', 'anmeldeformularanzeigen'])}
+  />
+)
+
+export default AktivitaetPagePreview
