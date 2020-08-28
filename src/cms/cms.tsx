1a049c8ab6bbcd2a67927fae31ae68afb1030b1f
Andrin Meier <andrin.meier@bmpi.ch>
2020-08-28 15:20:05 +0200

    rename pages because of duplicate names

--- src/cms/cms.tsx
+++ src/cms/cms.tsx
@@ -1,9 +1,11 @@
 import CMS from 'netlify-cms-app'
 import AktivitaetPagePreview from './preview-templates/AktivitaetPagePreview'
+import SpendenPagePreview from './preview-templates/SpendenPagePreview'
 var uploadcare = require('netlify-cms-media-library-uploadcare')
 var cloudinary = require('netlify-cms-media-library-cloudinary')
 
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
-CMS.registerPreviewTemplate('aktivitaeten', AktivitaetPagePreview)
+CMS.registerPreviewTemplate('aktivitaetpost', AktivitaetPagePreview)
+CMS.registerPreviewTemplate('spenden', SpendenPagePreview)
---
+++ src/cms/preview-templates/SpendenPagePreview.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SpendenPageTemplate } from '../../templates/spenden-page'
+
+const SpendenPagePreview = ({ entry, widgetFor }: {entry: any, widgetFor: any}) => (
+  <SpendenPageTemplate
+    title={entry.getIn(['data', 'title'])}
+    content={widgetFor('body')}
+    contentComponent={undefined}
+  />
+)
+
+export default SpendenPagePreview
