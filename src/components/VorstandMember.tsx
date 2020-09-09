70d9a7dc408fb35221645e311bb442b6e8d7ff0b
Andrin Meier <andrin.meier@hispeed.ch>
2020-09-09 12:17:05 +0200

    dark mode fixes

--- src/components/VorstandMember.tsx
+++ src/components/VorstandMember.tsx
@@ -28,9 +28,9 @@ const VorstandMember = ({
         <div className="media">
           <div className="media-content">
             <Link to={slug}><p className="title is-4">{title}</p></Link>
-            <p className="is-6" style={{ marginBottom: '0' }}>{funktion}</p>
-            <p className="is-6" style={{ marginBottom: '0' }}>Telefon: <a href={`tel:${telefon}`}>{telefon}</a></p>
-            <p className="is-6">E-Mail: <a href={`mailto:${email}`}>{email}</a></p>
+            <p className="is-6 dark-mode-content" style={{ marginBottom: '0' }}>{funktion}</p>
+            <p className="is-6 dark-mode-content" style={{ marginBottom: '0' }}>Telefon: <a href={`tel:${telefon}`}>{telefon}</a></p>
+            <p className="is-6 dark-mode-content">E-Mail: <a href={`mailto:${email}`}>{email}</a></p>
           </div>
         </div>
       </div>
