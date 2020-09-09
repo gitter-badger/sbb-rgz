3cbc0b082c36a6bbd35e566b1ee4ea8c3d81310f
Andrin Meier <andrin.meier@hispeed.ch>
2020-09-09 08:54:28 +0200

    remove margin

--- src/components/VorstandMember.tsx
+++ src/components/VorstandMember.tsx
@@ -28,8 +28,8 @@ const VorstandMember = ({
         <div className="media">
           <div className="media-content">
             <Link to={slug}><p className="title is-4">{title}</p></Link>
-            <p className="is-6">{funktion}</p>
-            <p className="is-6">Telefon: <a href={`tel:${telefon}`}>{telefon}</a></p>
+            <p className="is-6" style={{ marginBottom: '0' }}>{funktion}</p>
+            <p className="is-6" style={{ marginBottom: '0' }}>Telefon: <a href={`tel:${telefon}`}>{telefon}</a></p>
             <p className="is-6">E-Mail: <a href={`mailto:${email}`}>{email}</a></p>
           </div>
         </div>
