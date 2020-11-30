69e5c06c0454557fd838381630753d2de22582c2
Andrin Meier <andrin.meier@hispeed.ch>
2020-11-30 20:00:19 +0100

    lock adventskalender

--- src/functions/ist_adventskalender_tor_offen.js
+++ src/functions/ist_adventskalender_tor_offen.js
@@ -9,7 +9,7 @@ export function handler(event, context, callback) {
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({
-      isOpen: true,
+      isOpen: isOpen,
     }),
   });
 }
--- src/functions/welche_adventskalender_tore_sind_offen.js
+++ src/functions/welche_adventskalender_tore_sind_offen.js
@@ -2,14 +2,13 @@ export function handler(event, context, callback) {
   const days = [];
   const now = new Date();
   for (let i = 1; i <= 24; i++) {
-    /*if (
+    if (
       now.getFullYear() === 2020 &&
       now.getMonth() === 11 &&
       i <= now.getDate()
     )
       days.push(true);
-    else days.push(false);*/
-    days.push(true);
+    else days.push(false);
   }
   callback(null, {
     statusCode: 200,
